188c21db3e5b3b0cb8912ec0404e4edab03914f4
Jackson Christopher <kitchen.king.95@gmail.com>
2022-03-24 16:02:39 -0500

    j1

---
+++ client/client/src/components/Menu.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import './menu.css'
+import Checkout from './Checkout'
+
+const Menu = (props) => {
+
+  let [total, setTotal] = useState(0)
+ 
+  const addToTotal = (price) => {
+    setTotal(total += price)
+    console.log(total);
+  }
+
+  const subFromTotal = (price) => {
+    if (total === 0) {
+      <></>
+    } else
+    setTotal(total -= price)
+    console.log(total)
+  }
+
+  return (
+    <div>
+      <header>
+      <h2>Menu</h2>
+      </header>
+         <div>
+        { props.foods.map((food) => (        
+            <div key={food.id}>
+              <img style={{width: '200px'}} src={ food.img } />
+              <h2>{ food.name }</h2>
+              <p>{ food.description }</p>
+              <button onClick={() => addToTotal(food.price)} className='btn'>+</button>
+              <h3>${ food.price }</h3>
+              <button onClick={() => subFromTotal(food.price)} className='btn'>-</button>
+            </div> ))}
+        </div>
+
+        <footer>
+        <Checkout total={total} />
+        </footer>
+    </div>
+  )
+}
+
+export default Menu
