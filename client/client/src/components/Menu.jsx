cdb1be030952b5e27abd9f3697fe1679a4571927
Samuel Kibathi <kibathi2180@gmail.com>
2022-03-24 14:24:38 -0400

    fixed syntax error

--- client/client/src/components/Menu.jsx
+++ client/client/src/components/Menu.jsx
@@ -1,22 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './menu.css'
 import Checkout from './Checkout'
 
-const Menu = (props) => {
+const Menu = () => {
 
-  // let total = 0
 
-  // const calcTotal = (food) => {
-  //   total += {food.price}
-  // }
-
-  // const [total, setTotal] = useState(0)
-
-  // const addToTotal = (food.price) => {
-
-  //   setTotal(total += {parseInt(food.price)})
-
-  // }
   let total = 0
 
   const addToTotal = (price) => {
@@ -34,21 +22,21 @@ const Menu = (props) => {
       <h2>Menu</h2>
          <div>
         
-            <div key={food.id}>
+            {/* <div key={food.id}>
               <img style={{width: '200px'}} src={ food.img } />
               <h2>{ food.name }</h2>
               <p>{ food.description }</p>
               <button onClick={() => addToTotal(food.price)} className='btn'>+</button>
               <p>${ food.price }</p>
               <button onClick={() => subFromTotal(food.price)} className='btn'>-</button>
-            </div>
+            </div> */}
             <div>
               <img src="https://upload.wikimedia.org/wikipedia/commons/4/47/Hamburger_%28black_bg%29.jpg" alt="1" />
               <h2>HamBurger</h2>
               <p>1/4lb ground beef patty, lettuce, tomato, pickles, mustard, ketchup, between sesame seed buns.</p>
             </div> 
           ))
-          }
+          
           {
             <div>
               <img src="https://assets.myfoodandfamily.com/adaptivemedia/rendition/195370-3000x2000.jpg?id=093000b4880e99e6cd87fa511235a789145c5a0a&ht=650&wd=1004&version=1&clid=pim" alt="2" />
