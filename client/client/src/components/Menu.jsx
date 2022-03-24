1629f6ba47197d65d729eadbefc0b724c8fb2f95
Samuel Kibathi <kibathi2180@gmail.com>
2022-03-24 10:43:31 -0400

    added menu items to menu component

--- client/client/src/components/Menu.jsx
+++ client/client/src/components/Menu.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
-const Home = () => {
+const Menu = (props) => {
   return (
     <div>
-        
+      <div>
+        {
+          props.foods.map((food) => (
+            <div>
+              <img src={food.img} />
+              <h2>{ food.name }</h2>
+              <p>{ food.description }</p>
+              
+            </div>
+          ))
+        }
+        </div>
     </div>
   )
 }
 
-export default Home
+export default Menu
---
+++ client/client/src/data/foods.js
@@ -0,0 +1,6 @@
+export default [
+    {id: 1, menuItem: 'Hmburger', img: 'https://upload.wikimedia.org/wikipedia/commons/4/47/Hamburger_%28black_bg%29.jpg', description: 'Not made from real ham', price: 3},
+    {id: 2, menuItem: 'Hamburger With Cheese', img: 'https://assets.myfoodandfamily.com/adaptivemedia/rendition/195370-3000x2000.jpg?id=093000b4880e99e6cd87fa511235a789145c5a0a&ht=650&wd=1004&version=1&clid=pim', description: 'Not made from real ham. Now with cheese!', price: 4},
+    {id: 3, menuItem: 'Hotdog', img: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Hot_dog_with_mustard.png', description: 'Hot Diggity Dog', price: 3},
+  ]
+  
