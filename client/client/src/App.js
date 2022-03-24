648c8fc4b27b3ed456eecd35a0bd0dd647816d37
Mark Ryan <markrryan99@gmail.com>
2022-03-24 09:11:13 -0700

    axios connected

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,9 +1,20 @@
 import axios from 'axios'
-import { BASE_URL } from './globals'
-axios.get(`${BASE_URL}/foods`)
+import { useState, useEffect } from 'react'
+const BASE_URL = 'http://localhost:3001/api'
+
 
 function App() {
   
+  
+  useEffect(() => {
+    async function getFoods() {
+      const res = await axios.get(`${BASE_URL}/foods`)
+      console.log(res.data)
+    }
+    getFoods()
+  })
+
+
   return (
     <div className="App">
       
